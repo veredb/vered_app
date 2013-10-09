04f42639d49612887bbd026351667d66a1c3e4fb
Vered Bauer <veredbauer@Vereds-MacBook-Air.local>
2013-10-09 13:15:45 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VeredApp::Application.config.secret_key_base = 'ceab54c2a4ff686b04c70e07133b6bcfdecf0c9703424994aab9b51dbbb8f06dd3cb387d43a27ae334c5e314f920b9a602bd863d9fb90d21c883819ff7a70c01'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VeredApp::Application.config.session_store :cookie_store, key: '_vered_app_session'
