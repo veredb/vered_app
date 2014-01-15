7760ecec309e6c57c422c6bd91c4a77f1563aa60
Vered Bauer <veredbauer@Vereds-MacBook-Air.local>
2014-01-15 10:35:17 -0600

    tests added

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,46 @@
 require 'spec_helper'
 
 describe PostsController do
+   describe "GET 'new'" do
+
+       it "should be successful" do
+           get 'new'
+           response.should be_success
+       end
+   end
+   describe "GET 'show'" do
+
+       before(:each) do
+         @post = Factory(:post)
+       end
+
+       it "should be successful" do
+           get :show, :id => @post
+           response.should be_success
+       end
+
+       it "should find the right user" do
+           get :show, :id => @post
+           assigns(:post).should == @post
+       end
+        
+   end
+
+   describe "GET 'create'" do
+          before(:each) do
+              @attr = { :title => "Golf", :text => "I love golf" }
+          end
+
+          it "should create a post" do
+            lambda do
+              post :create, :post => @attr
+            end.should change(Post, :count).by(1)  
+          end
+
+          it "should redirect to the post show page" do
+             post :create, :post => @attr
+             response.should redirect_to(post_path(assigns(:post)))
+          end
+   end
 
 end
